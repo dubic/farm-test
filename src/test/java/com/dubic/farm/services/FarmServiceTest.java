c19caed8a0927e1caa4db10e796735a0285b934a
Dubem Uzuegbu <Dubem.Uzuegbu@Interswitchng.com>
2024-05-26 12:50:26 +0100

    all te

--- src/test/java/com/dubic/farm/services/FarmServiceTest.java
+++ src/test/java/com/dubic/farm/services/FarmServiceTest.java
@@ -2,6 +2,8 @@ package com.dubic.farm.services;
 
 import com.dubic.farm.api.requests.HarvestRequest;
 import com.dubic.farm.api.requests.PlantRequest;
+import com.dubic.farm.api.responses.CropReport;
+import com.dubic.farm.api.responses.FarmReport;
 import com.dubic.farm.models.Crop;
 import com.dubic.farm.models.CropType;
 import com.dubic.farm.models.Farm;
@@ -10,11 +12,13 @@ import com.dubic.farm.repositories.FarmRepo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -57,5 +61,47 @@ class FarmServiceTest {
         //then
         assertThat(saved, notNullValue());
         assertThat(saved, is(CropType.CORN));
+    }
+
+    @Test
+    void saveHarvested_no_farm_or_crop() {
+        //given
+        given(cropRepo.findByFarmAndType("farm a", CropType.CORN.name())).willReturn(Optional.empty());
+        given(cropRepo.save(any(Crop.class))).willReturn(new Crop(1L, CropType.CORN.name(), "farm a", 2023, 6, 2, 0));
+        //when
+        assertThrows(IllegalArgumentException.class, () ->
+                this.farmService.saveHarvested(new HarvestRequest(CropType.CORN, "farm a", 2, 2023)));
+    }
+
+    @Test
+    void farmReports() {
+        //given
+        given(cropRepo.getFarmReports()).willReturn(List.of(
+                new FarmReport(2023, "farm a", 16, 12)
+        ));
+        //when
+        List<FarmReport> farmReports = this.farmService.getFarmReports();
+        assertThat(farmReports, notNullValue());
+        assertThat(farmReports.size(), is(1));
+        assertThat(farmReports.get(0).getFarm(), is("farm a"));
+        assertThat(farmReports.get(0).getTotalActual(), is(12));
+        assertThat(farmReports.get(0).getTotalExpected(), is(16));
+        assertThat(farmReports.get(0).getSeason(), is(2023));
+    }
+
+    @Test
+    void cropReports() {
+        //given
+        given(cropRepo.getCropReports()).willReturn(List.of(
+                new CropReport(2023, "CORN", 16, 12)
+        ));
+        //when
+        List<CropReport> cropReports = this.farmService.getCropReports();
+        assertThat(cropReports, notNullValue());
+        assertThat(cropReports.size(), is(1));
+        assertThat(cropReports.get(0).getCrop(), is("CORN"));
+        assertThat(cropReports.get(0).getActual(), is(12));
+        assertThat(cropReports.get(0).getExpected(), is(16));
+        assertThat(cropReports.get(0).getSeason(), is(2023));
     }
 }
